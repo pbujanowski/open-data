8ccebd8dcb9e281da3216be0417109866a4a09a3
Patryk Bujanowski <bujanowski.patryk@outlook.com>
2022-02-04 16:36:10 +0100

    Added Material UI library.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { Button } from "@mui/material";
+
 const App: React.FC = () => {
-  return <h1>Hello world!</h1>;
+  return <Button variant="contained">Hello world!</Button>;
 };
 
 export default App;
