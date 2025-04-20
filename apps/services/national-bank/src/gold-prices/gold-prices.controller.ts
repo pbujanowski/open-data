e285839336b8129333937ec39c6875ebfb0a193d
Patryk Bujanowski <patryk.bujanowski@hotmail.com>
2025-04-20 19:35:23 +0200

    Added "Swagger" to "National Bank" service.

--- apps/services/national-bank/src/gold-prices/gold-prices.controller.ts
+++ apps/services/national-bank/src/gold-prices/gold-prices.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { GoldPricesService } from './gold-prices.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CurrentGoldPriceResponseDto } from './dto/responses/current-gold-price-response.dto';
 
+@ApiTags('gold-prices')
 @Controller('gold-prices')
 export class GoldPricesController {
   constructor(private readonly goldPricesService: GoldPricesService) {}
 
   @Get('current')
+  @ApiResponse({
+    status: 200,
+    description: 'Fetches the current gold price',
+    type: CurrentGoldPriceResponseDto,
+  })
   getCurrentGoldPrice() {
     return this.goldPricesService.getCurrentGoldPrice();
   }
--- apps/services/national-bank/src/main.ts
+++ apps/services/national-bank/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('National Bank API')
+    .setDescription('API documentation for the National Bank service')
+    .setVersion('1.0')
+    .addTag('gold-prices')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
