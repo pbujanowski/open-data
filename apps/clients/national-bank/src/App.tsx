77cc5ddb23560536380e80751c2aff76b6424091
Patryk Bujanowski <patryk.bujanowski@hotmail.com>
2025-05-01 16:36:19 +0200

    Created "National Bank" client app.

---
+++ apps/clients/national-bank/src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>Open Data National Bank Client</h1>
+      <p>This is a client application for the Open Data National Bank.</p>
+    </div>
+  );
+};
